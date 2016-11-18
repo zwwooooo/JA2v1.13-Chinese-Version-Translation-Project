d10d1c8dcbb7c01104fd9cad8399c418c6a61208
zwwooooo <zwwooooo@gmail.com>
2016-11-18 17:16:12 +0800

    2013/10.12/11:02:16 重新整理，准备新的汉化

---
+++ Data-UC113_TBS_Chinese/Scripts/Chinese.undergroundsectornames.lua
@@ -0,0 +1,66 @@
+﻿--[[
+
+    This file (or its localized versions respectively) are included in
+    "initunderground.lua" to provide the underground sector names.
+    
+    Make sure to save this file as UTF-8, preferably with BOM signature in
+    order to help editors to not mess up the encoding. Also make sure to not
+    save BOM in any other script unless it explicitly allows that.
+
+]]
+
+sectornames = {
+
+    ["A10-1"] = "Atremo UBT",
+    
+    ["J9-1"] = "Prax UBT",
+    ["J9-2"] = "Tixa Dungeon",
+    
+    ["K4-1"] = "Roxx UBT",
+    
+    ["O3-1"] = "Adrian UBT",
+    
+    ["P3-1"] = "Shelter",
+    
+    
+    ["B2-1"] = "Sheraton UBT",
+    
+    ["D4-1"] = "San Mona Mine",
+    ["D5-1"] = "San Mona Mine",
+    
+    ["D13-1"] = "Calisto UBT",
+    ["E13-1"] = "Factory Basement",
+    ["E13-2"] = "Drassen Mine",
+    ["F13-2"] = "Drassen Mine",
+    ["G13-2"] = "A.O. Office",
+    ["G13-3"] = "Drassen Mine",
+    ["F13-3"] = "Crepaton Excavation",
+    
+    ["H8-1"] = "Galileo UBT",
+    ["H9-1"] = "Galileo UBT",
+    ["H9-2"] = "Cambria Mine",
+    ["H8-2"] = "Cambria Mine",
+    ["H8-3"] = "Cambria Mine",
+    ["I8-3"] = "Cambria Mine",
+    ["J8-3"] = "Cambria Mine",
+
+    ["I14-1"] = "Troy UBT",
+    ["J14-1"] = "Troy UBT",
+    ["J14-2"] = "Alma Mine",
+    ["J13-2"] = "Alma Mine",
+    ["J13-3"] = "Alma Mine",
+    ["K13-3"] = "Alma Mine",
+    
+    ["H3-1"] = "Drake UBT",
+    ["I3-1"] = "Drake UBT",
+    ["I3-2"] = "Abandoned UBT",
+    ["H3-2"] = "Port Kip UBT",
+    ["H4-2"] = "Port Kip UBT",
+    ["H4-3"] = "Grumm Mine",
+    ["G4-3"] = "Grumm Mine",
+    
+    ["P1-1"] = "Demoville",
+    ["P1-2"] = "Demoville",
+ 
+    creatureLair = "Creature Lair",
+}
